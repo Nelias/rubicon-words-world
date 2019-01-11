b3fed6308d7a9695f5c2e907cea04b9e6d6e2bb9
Michał Antropik <michal.antropik@gmail.com>
2019-01-11 23:07:18 +0100

    code refactoring

--- rubicon_words_world.rb
+++ rubicon_words_world.rb
@@ -34,47 +34,42 @@ demon3 = <<DEMON
         wwwwwwwww
 DEMON
 
-def cl(seconds)
+def clean_sleep(seconds)
   sleep(seconds)
   system("clear")
 end
 
 puts File.readlines 'game_banner.txt'
 
-cl(3)
+clean_sleep(3)
 
 puts "May all those phonemes not drive you crazy!!!"
 
-cl(1.5)
+clean_sleep(1.5)
 
 3.times do
   puts "w, u, v, t, d, p, b, a, o, i, e, k, g, h, s, z"
 
-  cl(0.2)
+  clean_sleep(0.2)
   sleep(0.2)
 end
 
 puts "w, u, v, t, d, p, b, a, o, i, e, k, g, h, s, z"
-sleep(1)
-system("clear")
+
+clean_sleep(1)
 
 # conjure phoneme demon
 4.times do
   puts demon1
-  sleep(0.05)
-  system("clear")
+  clean_sleep(0.05)
   puts demon2
-  sleep(0.05)
-  system("clear")
+  clean_sleep(0.05)
   puts demon3
-  sleep(0.05)
-  system("clear")
+  clean_sleep(0.05)
   puts demon2
-  sleep(0.05)
-  system("clear")
+  clean_sleep(0.05)
   puts demon1
-  sleep(0.05)
-  system("clear")
+  clean_sleep(0.05)
 end
 
 puts "What word would you like to reveal?"
