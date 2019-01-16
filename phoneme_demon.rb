50d659299471a68541f75e074dc38773b207397a
Michał Antropik <michal.antropik@gmail.com>
2019-01-16 18:18:33 +0100

    Update phoneme_demon.rb

--- phoneme_demon.rb
+++ phoneme_demon.rb
@@ -1,1 +1,35 @@
+demon1 = <<DEMON
+  w h d           b h a
+    u   t       p   o
+      v v       e e
 
+          s   z
+
+        wwwwwwwww
+ 
+ 
+        wwwwwwwww
+DEMON
+
+demon2 = <<DEMON
+  u k t           p k o
+    v   d       b   a
+      w w       i i
+
+          z   s
+
+        wwwwwwwww
+ 
+        wwwwwwwww
+DEMON
+
+demon3 = <<DEMON
+  w h d           b h a
+    u   t       p   o
+      v v       e e
+
+          s   z
+
+        wwwwwwwww
+        wwwwwwwww
+DEMON
