1ec78b9dceec1458db8ece601c56f552e58178bc
Michał Antropik <michal.antropik@gmail.com>
2019-01-16 18:18:15 +0100

    Update rubicon_words_world.rb

--- rubicon_words_world.rb
+++ rubicon_words_world.rb
@@ -1,39 +1,3 @@
-demon1 = <<DEMON
-  w h d           b h a
-    u   t       p   o
-      v v       e e
-
-          s   z
-
-        wwwwwwwww
- 
- 
-        wwwwwwwww
-DEMON
-
-demon2 = <<DEMON
-  u k t           p k o
-    v   d       b   a
-      w w       i i
-
-          z   s
-
-        wwwwwwwww
- 
-        wwwwwwwww
-DEMON
-
-demon3 = <<DEMON
-  w h d           b h a
-    u   t       p   o
-      v v       e e
-
-          s   z
-
-        wwwwwwwww
-        wwwwwwwww
-DEMON
-
 def clean_sleep(seconds)
   sleep(seconds)
   system("clear")
