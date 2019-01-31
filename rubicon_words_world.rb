12cb0860708be468a54888dbf91c8e4ac95dfb82
Michał Antropik <michal.antropik@gmail.com>
2019-01-31 22:39:56 +0100

    update algorithm

--- rubicon_words_world.rb
+++ rubicon_words_world.rb
@@ -32,38 +32,69 @@ puts "What word would you like to reveal?"
 word = gets.chomp
 puts
 
+def additional_iteration(one, two, three, word, i)
+  
+  word.length.times do |j|
+    j += i + 1
+
+    if word[j] == two
+      word[j] = one
+      puts word
+
+      word[j] = three
+      puts word
+      next
+    end
+
+    if word[j] == one
+      word[j] = three
+      puts word
+
+      word[j] = two
+      puts word
+      next
+    end
+
+    if word[j] == three
+      word[j] = one
+      puts word
+
+      word[j] = two
+      puts word
+      next
+    end
+  end
+  
+end
+
+def phoneme_demon(one, two, three, word, i)
+
+  if word[i] == one
+    additional_iteration(one, two, three, word, i)
+
+    word[i] = two
+    puts word
+
+    additional_iteration(one, two, three, word, i)
+
+    word[i] = three
+    puts word
+
+    additional_iteration(one, two, three, word, i)
+  end
+
+end
+
 word.length.times do |i|
-  if word[i] == "h"
-    word[i] = "s"
-    puts word
+  phoneme_demon("h", "k", "s", word, i)
+end
 
-    word[i] = "k"
-    puts word
-    next
-  end
+word.length.times do |i|
+  phoneme_demon("k", "s", "h", word, i)
+end
 
-  if word[i] == "s"
-    word[i] = "k"
-    puts word
-
-    word[i] = "h"
-    puts word
-    next
-  end
-
-  if word[i] == "k"
-    word[i] = "s"
-    puts word
-
-    word[i] = "h"
-    puts word
-    next
-  end
-
-  if word[i] == "a"
-    word[i] = "o"
-    puts word
-  end
+word.length.times do |i|
+  phoneme_demon("s", "h", "k", word, i)
 end
 
 puts
